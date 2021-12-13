d2e4de16963b313cabb2def6431a41026d39dd9d
Wadie-ess <Walidehanin@gmail.com>
2021-12-13 21:33:20 +0100

    wlh ma 3rft ach gales kan khawer

--- map.c
+++ map.c
@@ -2,15 +2,45 @@
 #include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
+int ft_count_lines()
+{
+    int line_count;
+    line_count = 0;
+    int fd;
+    fd = open("map.ber", O_RDONLY);
+    while (get_next_line(fd) != NULL)
+        line_count++;
+    return line_count;
+}
+
 int main()
 {
-    int line_count = 1;
+
+    int line_count;
+    int colum_count;
+    int i;
+    int j;
+    i = 0;
+    int reded;
+    line_count = ft_count_lines();
+    //colum_count = ft_count_chars();
     int fd = open("map.ber", O_RDONLY);
-    int colum_count = strlen(get_next_line(fd));
-    printf("len dyal l file is %d", colum_count);
-    while (get_next_line(fd) != NULL)
-    {
-        line_count++;
+     //int fd2 = open("map.ber", O_RDONLY);
+    char **map;
+    map = malloc(line_count * sizeof(char *));
+    while (i < line_count)
+    {   
+        map[i] = get_next_line(fd);
+        i++;
     }
+    i = 0;
+
+    while (i < line_count)
+    {
+        printf("%s",map[i]);
+        i++;
+    }
+    
+    printf("len dyal l file is %d", colum_count);
     printf("che7al mn star is %d", line_count);
 }
