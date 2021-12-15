ad877d265d9808d3f9e5d90d0d7637a601b30da8
Wadie-ess <Walidehanin@gmail.com>
2021-12-15 20:57:54 +0100

    going on that shit

--- main.c
+++ main.c
@@ -1,19 +1,46 @@
 #include "so_long.h"
-int main()
+#include<mlx.h>
+#include "get_next_line.h"
+ void	ft_set_map(char *map_path, t_game *game)
 {
+	int		lines_count;
+	int		fd;
+	int		i;
 
+	lines_count = 0;
+	fd = open(map_path, O_RDONLY);
+	while (get_next_line(fd))
+		lines_count++;
+	game->map = malloc(sizeof(char *) * (lines_count));
+	close(fd);
+	i = -1;
+	fd = open(map_path, O_RDONLY);
+	while (++i < lines_count && game->map)
+		game->map[i] = get_next_line(fd);
+	close(fd);
+	game->lines_count = lines_count;
+	if (game->map)
+		game->chars_count = ft_strlen(game->map[i - 1]);
+} 
+
+int main()
+{   
+    t_game *game;
+    t_mlx  *mlx;
+    game = malloc(sizeof(t_game));
+    mlx = malloc(sizeof(t_mlx));
     int width;
     int height;
+    ft_set_map(MAP_PATH, game);
+    //char **map = game->map;
+   // printf("%d",game->chars_count);
+    height = game->lines_count * BLOCK_SIZE;
+    width = game->chars_count * BLOCK_SIZE;
+    mlx->mlx = mlx_init();
+    mlx->mlx_window = mlx_new_window(mlx->mlx,width, height, "a7a");
 
-    int lines_count = ft_count_lines();
-    int chars_count = ft_count_chars();
-    height = lines_count * BLOCK_SIZE;
-    width = chars_count * BLOCK_SIZE;
-    void *mlx = mlx_init();
-    void *mlx_window = mlx_new_window(mlx ,width, height, "a7a");
+    ft_draw_map(game->lines_count, game->chars_count, mlx->mlx, mlx->mlx_window);
+    mlx_loop(mlx->mlx);
 
-    ft_draw_map(lines_count, chars_count, mlx, mlx_window);
-    mlx_loop(mlx);
-
-    return (0);
+    return (0);  
 }
